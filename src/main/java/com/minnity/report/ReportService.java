7a24a4d2db07f1d5241cb70426901d19a7506918
kshitizbista <tsetiz.bista@gmail.com>
2020-10-26 22:58:46 +0100

    Add implementation for methods calculateNumberOfRequestsPerCompany, findRequestsWithError and findRequestPathWithLongestDurationTime in ReportService class

--- src/main/java/com/minnity/report/ReportService.java
+++ src/main/java/com/minnity/report/ReportService.java
@@ -1,24 +1,43 @@
 package com.minnity.report;
 
-import org.apache.commons.lang3.NotImplementedException;
-
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ReportService {
 
-  //task 1: Return number of requests that were made for each company. (e.g. companyId -> requestNumber)
-  public Map<Integer, Long> calculateNumberOfRequestsPerCompany(List<RequestLog> requestLogs) {
-    throw new NotImplementedException("TODO implement this method");
-  }
+    /**
+     * Return number of requests that were made for each company
+     *
+     * @param requestLogs must not be {@literal null}.
+     * @return Map with key as company id and value as total request made by company (e.g. companyId -> requestNumber)
+     */
+    public Map<Integer, Long> calculateNumberOfRequestsPerCompany(List<RequestLog> requestLogs) {
+        return requestLogs.stream()
+                .collect(Collectors.groupingBy(RequestLog::getCompanyId, Collectors.counting()));
+    }
 
-  //task 2: Count and return requests per company that finished with an error HTTP response code (>=400)
-  public Map<Integer, RequestLog> findRequestsWithError(List<RequestLog> requestLogs) {
-    throw new NotImplementedException("TODO implement this method");
-  }
+    /**
+     * Count and return requests per company that finished with an error HTTP response code (>=400)
+     *
+     * @param requestLogs must not be {@literal null}.
+     * @return Map with key as company id and value as Lists of RequestLog made by company (e.g. companyId -> List<RequestLog>)
+     */
+    public Map<Integer, List<RequestLog>> findRequestsWithError(List<RequestLog> requestLogs) {
+        return requestLogs.stream()
+                .filter(requestLog -> requestLog.getRequestStatus() >= 400)
+                .collect(Collectors.groupingBy(RequestLog::getCompanyId));
+    }
 
-  //task 3: find and print API (requests path) that on average takes the longest time to process the request.
-  public String findRequestPathWithLongestDurationTime(List<RequestLog> requestLogs) {
-    throw new NotImplementedException("TODO implement this method");
-  }
+    /**
+     * Find and get API (requests path) that on average takes the longest time to process the request.
+     *
+     * @param requestLogs must not be {@literal null}.
+     * @return the request url having max request duration time
+     */
+    public String findRequestPathWithLongestDurationTime(List<RequestLog> requestLogs) {
+        return Collections.max(requestLogs, Comparator.comparingLong(RequestLog::getRequestDuration))
+                .getRequestPath();
+    }
+
+
 }
